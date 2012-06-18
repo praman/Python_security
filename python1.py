e7181a64820b54c9c97624d76aad1ef2ea31f846
padmanabhan raman <praman131@gmail.com>
2012-06-18 12:47:41 +0530

    made some changes

--- python1.py
+++ python1.py
@@ -4,3 +4,13 @@ while count < len(movies):
       print(movies[count])
       count=count+1
       
+#this is my first python program
+import sys
+print(sys.platform)
+x=2**10
+print x
+z='SEro'
+print(z*20)
+#a=input()
+#a= input()
+#b=raw_input()	
